b7ddab25d804fa44d50a8cf9d5b4b3148ec91365
tobexint <tobexint@gmail.com>
2022-09-04 01:29:37 -0700

    console file

--- console.py
+++ console.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Defines the entry point of the command interpreter """
+""" Defines the entry point of the command interpreter. """
 import cmd
 import json
 from models import storage
